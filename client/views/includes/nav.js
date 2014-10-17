680b8005e18ff00bab28fcaace4062f43be505e1
hmcc <heather@recoil.org>
2014-10-17 10:14:44 +0100

    Added logout link.

--- client/views/includes/nav.js
+++ client/views/includes/nav.js
@@ -3,5 +3,20 @@ Template.nav.helpers({
     var currentRoute = Router.current();
     return currentRoute &&
       template === currentRoute.lookupTemplate() ? 'active' : '';
+  },
+
+  displayName: function(template) {
+    var user = Meteor.user();
+
+    if (!user)
+      return '';
+    if (user.profile && user.profile.name)
+      return user.profile.name;
+    if (user.username)
+      return user.username;
+    if (user.emails && user.emails[0] && user.emails[0].address)
+      return user.emails[0].address;
+
+      return '';
   }
 });
