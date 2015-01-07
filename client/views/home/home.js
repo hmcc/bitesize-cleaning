2f0e370aafcba851065b1b69277d918be864f872
hmcc <heather@recoil.org>
2015-01-07 22:15:05 +0000

    Prototype for new home screen with today's chores.

--- client/views/home/home.js
+++ client/views/home/home.js
@@ -1,5 +1,13 @@
 Template.home.helpers({
+  today: function(template) {
+    return new Date().toDateString();
+  },
   tasks: function() {
-    return Tasks.find();
+    var today = new Date();
+    today.setHours(0);
+    today.setMinutes(0);
+    today.setSeconds(0);
+    today.setMilliseconds(0);
+    return Tasks.find({date: today});
   }
 });
--- server/fixtures.js
+++ server/fixtures.js
@@ -1,14 +1,64 @@
 if (Tasks.find().count() === 0) {
   Tasks.insert({
     title: 'Hoover downstairs',
-    time: '10 minutes',
-    user: 'heather'
+    time: 10,
+    date: [new Date('2015-01-05'), new Date('2014-01-09')]
   });
 
   Tasks.insert({
-    title: 'Clean kitchen sink',
-    time: '5 minutes',
-    user: 'dave'
+    title: 'Downstairs toilet',
+    time: 10,
+    date: [new Date('2015-01-05')]
+  });
+
+  Tasks.insert({
+    title: 'Take out rubbish',
+    time: 10,
+    date: [new Date('2015-01-06')]
+  });
+
+  Tasks.insert({
+    title: 'Empty bins',
+    time: '10 minutes',
+    date: [new Date('2015-01-06')]
+  });
+
+  Tasks.insert({
+    title: 'Rufus bedding change',
+    time: 5,
+    date: [new Date('2015-01-07')]
+  });
+
+  Tasks.insert({
+    title: 'Empty bathroom bin',
+    time: 5,
+    date: [new Date('2015-01-07')]
+  });
+
+  Tasks.insert({
+    title: 'Hoover upstairs',
+    time: 10,
+    date: [new Date('2015-01-08')]
+  });
+
+
+  Tasks.insert({
+    title: 'Upstairs bathroom',
+    time: '15 minutes',
+    date: [new Date('2015-01-08')]
+  });
+
+
+  Tasks.insert({
+    title: 'Oven',
+    time: '15 minutes',
+    date: [new Date('2015-01-10')]
+  });
+
+  Tasks.insert({
+    title: 'Fridge',
+    time: '15 minutes',
+    date: [new Date('2015-01-11')]
   });
 }
 
