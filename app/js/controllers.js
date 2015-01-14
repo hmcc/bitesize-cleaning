8a967d44bfdd3b31892f197575f43edc7cbfe762
hmcc <heather@recoil.org>
2015-01-14 22:43:39 +0000

    Show a full week's data instead of just one day.

--- app/js/controllers.js
+++ app/js/controllers.js
@@ -12,16 +12,57 @@ appControllers.controller('NavCtrl', ['$scope', '$location', function($scope, $l
 }]);
 
 appControllers.controller('TaskListCtrl', ['$scope', '$http', 'moment', function($scope, $http, moment) {
-  var today = moment().format('YYYY-MM-DD');
+  var today = moment();
+  
   $http.get('data/tasks.json').success(function(tasks) {
-    $scope.tasks = tasks.filter(function(task) {
-      return $.inArray(today, task.date) >= 0
+    $scope.tasks = new Array();
+
+    // Ugh. All this should go when there's a proper database 
+    // and can have the data in a more convenient format.
+    var taskLen = tasks.length;
+    for (var i = 0; i < taskLen; i++) {
+      var dateLen = tasks[i].date.length;
+      for (var j = 0; j < dateLen; j++) {
+        var taskDate = moment(tasks[i].date[j]);
+        if ((taskDate.day() === 0 && today.week() + 1 === taskDate.week()) || 
+            (taskDate.day() === 0 && today.day() === 0 && today.week() === taskDate.week()) ||
+            (taskDate.day() !== 0 && today.week() === taskDate.week())) {
+          var returnTask = {};
+          returnTask.date = taskDate;
+          returnTask.title = tasks[i].title;
+          returnTask.description = tasks[i].description;
+          returnTask.time = tasks[i].time;
+          $scope.tasks.push(returnTask);
+        }
+      }
+    }
+
+    // Sort the tasks chronologically
+    $scope.tasks.sort(function(a, b) {
+      if (a.date.isBefore(b.date)) {
+        return -1;
+      }
+      if (a.date.isAfter(b.date)) {
+        return 1;
+      }
+      return 0;
+    });
+
+    // Format the dates
+    $scope.tasks.map(function(val) {
+      val.date = val.date.format('dddd D MMM');
     });
   });
 
-  $scope.orderProp = 'title';
+  
+  $scope.orderProp = 'date';
 
-  $scope.today = moment().format('Do MMMM YYYY');
+  $scope.weekStart = moment();
+  if ($scope.weekStart.day() === 0)
+    $scope.weekStart.day(-6)
+  else 
+    $scope.weekStart.day(1)
+  $scope.weekStart = $scope.weekStart.format('Do MMMM YYYY');
 }]);
 
 appControllers.controller('BacklogCtrl', ['$scope', '$http', function($scope, $http) {
